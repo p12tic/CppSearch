f812ef06a34260842619513b73b085ffa5e17ff5
Marcin Mikołajczak <me@m4sk.in>
2017-06-22 10:44:26 +0200

    i18n: Polish translation
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

--- CppSearch.i18n.php
+++ CppSearch.i18n.php
@@ -14,3 +14,10 @@ $messages['en'] = array(
     'cppsearch_desc' => 'C/C++ keyword search extension ',
     'cppsearch_externalengines' => 'You can also use external search engines for the query: $1',
 );
+
+/** Polish 
+ */
+$messages['pl'] = array(
+    'cppsearch_desc' => 'Rozszerzenie wyszukiwania słów kluczowych C/C++ ',
+    'cppsearch_externalengines' => 'Możesz też użyć zewnętrznych silników wyszukiwania dla zapytania: $1',
+);
